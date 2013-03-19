2abf2f83b65864e17cfe5ef941a7c7554f4633af
Pieter Joost van de Sande <pj@born2code.net>
2013-03-19 08:38:35 +0100

    Add test for newline and comma separation combination

--- calculator_test.go
+++ calculator_test.go
@@ -31,6 +31,19 @@ func TestAddReturnsInputForSingleNumber(t *testing.T) {
 	}
 }
 
+func TestAddReturnsInputForTwoNumbers(t *testing.T) {
+	expected := 4
+
+	input := "2,2"
+	result := Add(input)
+
+	if result == expected {
+		t.Log("Add returns %v as expected for two number input separated by comma", result)
+	} else {
+		t.Errorf("Add returns %v on two number input separated by comma, %v is expected", result, expected)
+	}
+}
+
 func TestAddReturnsInputForTwoNumbersSeparatedByComma(t *testing.T) {
 	expected := 4
 
@@ -54,5 +67,18 @@ func TestAddReturnsInputForTwoNumbersSeparatedByNewline(t *testing.T) {
 		t.Log("Add returns %v as expected for two number input separated by newline", result)
 	} else {
 		t.Errorf("Add returns %v on two number input separated by newline, %v is expected", result, expected)
+	}
+}
+
+func TestAddReturnsInputForNumbersSeparatedByNewlineAndComma(t *testing.T) {
+	expected := 7
+
+	input := "2,3\n2"
+	result := Add(input)
+
+	if result == expected {
+		t.Log("Add returns %v as expected for numbers separated by newline and comma", result)
+	} else {
+		t.Errorf("Add returns %v for numbers separated by newline and comma, %v is expected", result, expected)
 	}
 }
